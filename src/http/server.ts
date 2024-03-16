1103df9cf1f499fbb20a3b953b606523b933e217
Yan Lyra <yanlyra3@gmail.com>
2024-03-16 14:39:43 -0300

    feat: started project

---
+++ src/http/server.ts
@@ -0,0 +1,12 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+  .get('/', () => {
+    return 'Hello World'
+  })
+
+app.listen(3333, () => {
+  console.log(
+    `🚀 Server is running on http://localhost:3333`
+  )
+})
