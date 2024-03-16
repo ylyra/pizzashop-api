e33b4cc5b319f7901579d5560976dc2d8f48281e
Yan Lyra <yanlyra3@gmail.com>
2024-03-16 17:35:52 -0300

    feat: add jwt and cookie to project

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { minLength, object, parse, string, url } from 'valibot'
 const envSchema = object({
   DATABASE_URL: string([url(), minLength(1)]),
   AUTH_REDIRECT_URL: string([url(), minLength(1)]),
+  JWT_SECRET_KEY: string([minLength(1)]),
 })
 
 export const env = parse(envSchema, process.env)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from 'elysia'
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import { Elysia, t } from 'elysia'
+import { env } from '../env'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log(`🚀 Server is running on http://localhost:3333`)
