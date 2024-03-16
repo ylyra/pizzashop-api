5f1c1fbca11a3cefe5699d27967eb96bdbd4bad5
Yan Lyra <yanlyra3@gmail.com>
2024-03-16 20:36:01 -0300

    feat(drizzle): create schemas for orders, order-items, and products

---
+++ src/db/schema/order-items.ts
@@ -0,0 +1,18 @@
+import { createId } from '@paralleldrive/cuid2'
+import { integer, pgTable, text } from 'drizzle-orm/pg-core'
+import { orders } from './orders'
+import { products } from './products'
+
+export const orderItems = pgTable('order_items', {
+  id: text('id').$defaultFn(createId).primaryKey(),
+  orderId: text('order_id')
+    .notNull()
+    .references(() => orders.id, {
+      onDelete: 'cascade',
+    }),
+  productId: text('product_id').references(() => products.id, {
+    onDelete: 'set null',
+  }),
+  priceInCents: integer('price_in_cents').notNull(),
+  quantity: integer('quantity').notNull(),
+})
